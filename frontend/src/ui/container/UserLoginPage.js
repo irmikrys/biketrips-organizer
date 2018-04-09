860a80fee69aed212c6374ac9eaf58c3864e009a
Niomika <domiryll@student.agh.edu.pl>
2018-04-10 00:04:23 +0200

    [US #9] Added Google login button

--- frontend/src/ui/container/UserLoginPage.js
+++ frontend/src/ui/container/UserLoginPage.js
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from "react-router";
+import {GoogleLogin} from 'react-google-login';
+import {PostData} from '../PostData';
+import {Redirect} from "react-router-dom";
 
 export class UserLoginPage extends Component {
 
-  render() {
+  render(){
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
+
     return (
       <div className="login-page">
         <div className="form-container">
           <form>
             <p className="message"><Link to="/admin_login">Log in as administrator</Link></p>
+            <GoogleLogin
+              clientId="54595961450-h0e06ev7237pca6i0742ecoif25ar04a.apps.googleusercontent.com"
+              class="g-signin"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+            />
           </form>
         </div>
       </div>
