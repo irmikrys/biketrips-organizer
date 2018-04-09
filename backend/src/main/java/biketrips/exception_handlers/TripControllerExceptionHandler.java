c111db111acf3aa20ab6f696a3940fa1b4253065
Irmina Krysiak <irmina.krysiak@gmail.com>
2018-04-09 20:37:30 +0200

    [US #19] Basic trip exceptions with handlers.

---
+++ backend/src/main/java/biketrips/exception_handlers/TripControllerExceptionHandler.java
@@ -0,0 +1,50 @@
+package biketrips.exception_handlers;
+
+import biketrips.controller.ApplicationsController;
+import biketrips.dto.ErrorMessage;
+import biketrips.exceptions.AccessingPrivateResourcesException;
+import biketrips.exceptions.TripException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice(assignableTypes = ApplicationsController.class)
+public class TripControllerExceptionHandler {
+  private final Log log = LogFactory.getLog(getClass());
+
+  @ExceptionHandler(TripException.class)
+  @ResponseBody
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ErrorMessage handleCreationException(TripException e) {
+    log.warn(e.getMessage());
+    return new ErrorMessage(e.getMessage());
+  }
+
+  @ExceptionHandler(AccessingPrivateResourcesException.class)
+  @ResponseBody
+  @ResponseStatus(HttpStatus.UNAUTHORIZED)
+  public ErrorMessage handleAccessingPrivateResourcesException(AccessingPrivateResourcesException e) {
+    log.warn(e.getMessage());
+    return new ErrorMessage(e.getMessage());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseBody
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ErrorMessage handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    log.warn(e.getMessage());
+    return new ErrorMessage("trip.error.badRequest");
+  }
+
+  @ExceptionHandler(Exception.class)
+  @ResponseBody
+  public ErrorMessage handleUnknownException(Exception e) {
+    return new ErrorMessage("tripService.error.unknownError");
+  }
+
+}
---
+++ backend/src/main/java/biketrips/exceptions/TripException.java
@@ -0,0 +1,13 @@
+package biketrips.exceptions;
+
+public class TripException extends RuntimeException {
+
+  public TripException() {
+    super();
+  }
+
+  public TripException(String message) {
+    super(message);
+  }
+
+}
