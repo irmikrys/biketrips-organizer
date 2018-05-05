3a1bf9adf06baaaab00d02381bae6e63e978cd85
Irmina Krysiak <irmina.krysiak@gmail.com>
2018-05-05 17:16:37 +0200

    [US #64] Adding dropzone to photos page.

--- frontend/src/ui/container/user/PhotosPage.js
+++ frontend/src/ui/container/user/PhotosPage.js
@@ -2,14 +2,58 @@ import React, {Component} from 'react';
 import {connect} from "react-redux";
 import {fetchPhotosByIdAlbum} from "../../../reducers/photos/photos";
 import {addPhotoToAlbum} from "../../../reducers/photos/addPhoto";
+import Dropzone from 'react-dropzone';
 
 class PhotosPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      accepted: [],
+      rejected: []
+    }
+  }
+
+  goToAlbums = () => {
+    window.location = '/trips/' + this.props.params.idTrip + '/albums'
+  };
+
   render() {
     return (
-      <div className='margin-top'>
-        <button className='btn btn-default'>
-          Add photo to album {this.props.params.idAlbum}
+      <div className='photos-page'>
+        <button className='btn btn-default'
+                onClick={this.goToAlbums}
+        >
+          Go back to trip albums
         </button>
+        <div className='photos-drop'>
+          <div className='drop'>
+            <Dropzone
+              className='dropzone'
+              accept="image/jpeg, image/png"
+              onDrop={(accepted, rejected) => {
+                this.setState({accepted, rejected});
+              }}
+            >
+              <p>Try dropping some files here, or click to select files to upload.</p>
+              <p>Only *.jpeg and *.png images will be accepted</p>
+            </Dropzone>
+          </div>
+          <div className='aside'>
+            <h2>Accepted files</h2>
+            <ul>
+              {
+                this.state.accepted.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+              }
+            </ul>
+            <h2>Rejected files</h2>
+            <ul>
+              {
+                this.state.rejected.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+              }
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
